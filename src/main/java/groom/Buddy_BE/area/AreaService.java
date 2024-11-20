f18601e7b36b41d749141a4d84c7e3a54b9ce039
GOOHAESEUNG <amu6675@naver.com>
2024-11-20 15:27:28 +0900

    feat : 완수한 영역 리스트 출력 #12

--- src/main/java/groom/Buddy_BE/area/AreaService.java
+++ src/main/java/groom/Buddy_BE/area/AreaService.java
@@ -1,11 +1,13 @@
 package groom.Buddy_BE.area;
 
-
+import groom.Buddy_BE.member.Member;
 import groom.Buddy_BE.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -42,4 +44,21 @@ public class AreaService {
 
         return area;
     }
+
+
+    //수행 완료한 영역 리스트 출력
+    public List<String> completeAreaTypes(Long kakaoId) {
+        Member member = memberService.findByKakaoId(kakaoId);
+
+        if (member == null) {
+            throw new IllegalArgumentException("회원이 존재하지 않습니다.");
+        }
+
+        // 해당 유저의 영역 중 isCompleted가 true인 영역의 AreaType 추출
+        return member.getAreas().stream()
+                .filter(Area::isCompleted) // isCompleted가 true인 영역만 필터링
+                .map(area -> area.getAreaType().name()) // AreaType을 문자열로 변환
+                .collect(Collectors.toList());
+    }
+
 }
